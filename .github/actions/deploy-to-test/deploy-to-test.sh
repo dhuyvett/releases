381fb52bac2095a20d30bab08539b7daeba20acc
David Dhuyvetter <ddhuyvetter@godaddy.com>
2022-10-13 07:43:51 -0700

    test

--- .github/actions/deploy-to-test/deploy-to-test.sh
+++ .github/actions/deploy-to-test/deploy-to-test.sh
@@ -10,6 +10,13 @@ fi
 ISSUE_NUMBER="$1"
 PR=$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$ISSUE_NUMBER")
 
-echo "$PR"
+SHA=$(echo "$PR" | jq -r ".merge_commit_sha")
+
+echo "SHA: $SHA"
+
+git fetch --all
+git checkout -b test origin/test
+git reset --hard "$SHA"
+git push --force origin test
 
 curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$ISSUE_NUMBER/labels" -d '{"labels":["Deployed to Test"]}'
