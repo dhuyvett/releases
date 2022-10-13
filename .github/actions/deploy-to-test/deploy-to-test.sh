4b14ee49ef52d016dd60d6c164e391df102fdb65
David Dhuyvetter <ddhuyvetter@godaddy.com>
2022-10-13 15:13:10 -0700

    this might even work

--- .github/actions/deploy-to-test/deploy-to-test.sh
+++ .github/actions/deploy-to-test/deploy-to-test.sh
@@ -3,21 +3,27 @@
 set -eu
 
 if [ $# -ne 1 ]; then
-    echo "1 parameters required: ISSUE_NUMBER"
-    exit 1
+  echo "1 parameters required: ISSUE_NUMBER"
+  exit 1
 fi
 
 ISSUE_NUMBER="$1"
-PR=$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$ISSUE_NUMBER")
+
+# Get the full PR details
+PR=$(curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$ISSUE_NUMBER")
 
 SHA=$(echo "$PR" | jq -r ".merge_commit_sha")
-
 echo "SHA: $SHA"
 
+# Switch to test branch and reset it to the merge commit SHA
 git fetch --all
 git checkout -b test origin/test
 git reset --hard "$SHA"
 
+# Create a deployment for the merge commit SHA
 DEPLOYMENT=$(curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
@@ -27,6 +33,7 @@ DEPLOYMENT=$(curl \
 
 DEPLOYMENT_ID=$(echo "$DEPLOYMENT" | jq -r ".id")
 
+# Mark the deployment as succeeded
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
@@ -34,6 +41,12 @@ curl \
   "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/deployments/$DEPLOYMENT_ID/statuses" \
   -d '{"environment":"test","state":"success"}'
 
+# force push the merce branch SHA to upstream test
 git push --force origin test
 
-curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$ISSUE_NUMBER/labels" -d '{"labels":["Deployed to Test"]}'
+# label the PR: Deployed to Test
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$ISSUE_NUMBER/labels" \
+  -d '{"labels":["Deployed to Test"]}'
